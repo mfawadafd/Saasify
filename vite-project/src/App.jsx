18bdf4fb361c2e2a1780f8f2e182d99843fa4a3b
Muhammad Fawad <mfawadafridi83@gmail.com>
2026-06-26 18:44:21 +0500

    ...

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import { NavLink } from 'react-router-dom'
@@ -6,12 +6,22 @@ import Hero from './components/Hero'
 import Features from './pages/Features'
 import Pricing from './pages/Pricing'
 import Testimonials from './pages/Testimonials'
+import { useLocation } from 'react-router-dom'
 
+function ScrollToTop(){
+  const {pathname} = useLocation();
+  useEffect(()=>{
+    window.scrollTo(0,0);
+  },[pathname])
+  return null;
+}
 
 const App = () => {
   return (
     <>
+
       <Navbar />
+      <ScrollToTop/>
       <main className='pt-20'>
         <Routes>
           <Route path='/' element={<Hero />}></Route>
